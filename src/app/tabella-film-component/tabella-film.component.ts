3881f565284cc66128850a0324f13b69927b1480
autovetturaincidentata <domenicogaudio34@gmail.com>
2024-12-22 16:52:54 +0100

    Aggiunta la selezione singola di stelline all'inserimento della riga con un prompt per il testing

--- src/app/tabella-film-component/tabella-film.component.ts
+++ src/app/tabella-film-component/tabella-film.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, input, ViewEncapsulation} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {NgForOf} from '@angular/common';
 
@@ -20,9 +20,9 @@ export class TabellaFilmComponent {
   addrow() {
     const groupName = `rating${this.count}`;
 
-    this.righe.push({
-      anno: 2025,
-      rating: `<input value="5" id="\`star${5}-${groupName}" name="\`${groupName}" type="radio">
+    const score=window.prompt("voto"); //dummy per testare l'aggiunzione di un voto custom
+
+    const starLayout=`<input value="5" id="\`star${5}-${groupName}" name="\`${groupName}" type="radio">
                <label for="\`star${5}-${groupName}"></label>
                <input value="4" id="\`star${4}-${groupName}" name="\`${groupName}" type="radio">
                <label for="\`star${4}-${groupName}"></label>
@@ -31,11 +31,26 @@ export class TabellaFilmComponent {
                <input value="2" id="\`star${2}-${groupName}" name="\`${groupName}" type="radio">
                <label for="\`star${2}-${groupName}"></label>
                <input value="1" id="\`star${1}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${1}-${groupName}"></label>`,
+               <label for="\`star${1}-${groupName}"></label>`;
+
+    let doc = new DOMParser().parseFromString(starLayout, "text/html");
+
+    doc.getElementsByTagName("input")[5-Number(score)].setAttribute("checked", "checked");
+    //il problema del reset delle stelline persiste ma comunque funziona, basta salvare lo score nel db e poi pullare da quello
+
+    const riga={
+      anno: 2025,
+      rating: doc.body.innerHTML,
       nome: "godzilla",
       immagine: "assets/images/img.png"
-    });
+    }
+
+    this.righe.push(riga);
 
     this.count++;
   }
+
+
+
+
 }
