5427707dc871de973701ca74e534942c6ca0932e
Piero23 <carlogozzi_cg@libero.it>
2024-12-27 14:46:15 +0100

    Struttura schermata Film

---
+++ src/app/schermata-film-component/schermata-film-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schermata-film-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './schermata-film-component.component.html',
+  styleUrl: './schermata-film-component.component.css'
+})
+export class SchermataFilmComponentComponent {
+
+}
