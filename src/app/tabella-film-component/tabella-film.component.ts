1af603d448d51a6b64fcd86d7ec5a042dd662a23
autovetturaincidentata <domenicogaudio34@gmail.com>
2024-12-30 00:13:08 +0100

    Ripulito il codice delle stelle utilizzando il binding di Angular e non più il javascript

--- src/app/tabella-film-component/tabella-film.component.ts
+++ src/app/tabella-film-component/tabella-film.component.ts
@@ -1,57 +1,37 @@
-import {Component, input, ViewEncapsulation} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {NgForOf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-tabella-film',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, FormsModule],
   templateUrl: './tabella-film.component.html',
-  styleUrls: ['./tabella-film.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./tabella-film.component.css']
 })
 export class TabellaFilmComponent {
 
-  count=0;
   nameOrder=true;
   yearOrder=true;
   ratingOrder=true;
 
-  righe :{anno: number, rating: string, numericalRating: number, nome: string, immagine: string}[] = [];
+  righe :{anno: number, rating: number, nome: string, immagine: string}[] = [];
 
-  constructor(protected sanitizer: DomSanitizer){}
+  constructor(private cdr: ChangeDetectorRef) {}
 
   addrow() {
-    const groupName = `rating${this.count}`;
-
     const score=window.prompt("voto"); //dummy per testare l'aggiunzione di un voto custom
-
-    const starLayout=`<input value="5" id="\`star${5}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${5}-${groupName}"></label>
-               <input value="4" id="\`star${4}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${4}-${groupName}"></label>
-               <input value="3" id="\`star${3}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${3}-${groupName}"></label>
-               <input value="2" id="\`star${2}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${2}-${groupName}"></label>
-               <input value="1" id="\`star${1}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${1}-${groupName}"></label>`;
-
-    let doc = new DOMParser().parseFromString(starLayout, "text/html");
-
-    doc.getElementsByTagName("input")[5-Number(score)].setAttribute("checked", "checked");
 
     const riga={
       anno: 2025,
-      rating: doc.body.innerHTML,
-      numericalRating: Number(score),
+      rating: Number(score),
       nome: "godzilla",
       immagine: "assets/images/img.png"
     }
 
-    this.righe.push(riga);
+    console.log(riga.rating);
 
-    this.count++;
+    this.righe.push(riga);
   }
 
   sortByName(){
@@ -79,37 +59,26 @@ export class TabellaFilmComponent {
     this.ratingOrder=true;
 
     if (this.yearOrder){
-      this.righe.sort(function cmp(a,b){
-        if (a.anno<b.anno) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => b.anno - a.anno)
       this.yearOrder=!this.yearOrder;
     }
     else{
-      this.righe.sort(function cmp(a,b){
-        if (a.anno>b.anno) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => a.anno - b.anno)
       this.yearOrder=!this.yearOrder;
     }
   }
 
   sortByRating() {
+    console.log(this.righe);
     this.nameOrder=true;
     this.yearOrder=true;
 
     if (this.ratingOrder){
-      this.righe.sort(function cmp(a,b){
-        if (a.numericalRating<b.numericalRating) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => b.rating - a.rating);
       this.ratingOrder=!this.ratingOrder;
     }
     else{
-      this.righe.sort(function cmp(a,b){
-        if (a.numericalRating>b.numericalRating) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => a.rating - b.rating);
       this.ratingOrder=!this.ratingOrder;
     }
   }
--- src/app/tabella-serie-tv-component/tabella-serie-tv.component.ts
+++ src/app/tabella-serie-tv-component/tabella-serie-tv.component.ts
@@ -1,61 +1,39 @@
-import {booleanAttribute, Component, ViewEncapsulation} from '@angular/core';
-import {NgForOf} from '@angular/common';
-import {DomSanitizer} from '@angular/platform-browser';
+import {Component} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-tabella-serie-tv',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf,
+    FormsModule
   ],
   templateUrl: './tabella-serie-tv.component.html',
-  styleUrl: './tabella-serie-tv.component.css',
-  encapsulation: ViewEncapsulation.None
+  styleUrl: './tabella-serie-tv.component.css'
 })
 
 export class TabellaSerieTvComponent {
 
-  count=0;
   nameOrder=true;
   yearOrder=true;
   ratingOrder=true;
 
-  righe :{anno: number, rating: string, numericalRating: number, nome: string, immagine: string, status: string}[] = [];
-
-  constructor(protected sanitizer: DomSanitizer){}
+  righe :{anno: number, rating: number, nome: string, immagine: string, status: string}[] = [];
 
   addrow() {
-    const groupName = `rating${this.count}`;
-
     const score = window.prompt("voto"); //dummy per testare l'aggiunzione di un voto custom
-
-    const starLayout = `<input value="5" id="\`star${5}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${5}-${groupName}"></label>
-               <input value="4" id="\`star${4}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${4}-${groupName}"></label>
-               <input value="3" id="\`star${3}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${3}-${groupName}"></label>
-               <input value="2" id="\`star${2}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${2}-${groupName}"></label>
-               <input value="1" id="\`star${1}-${groupName}" name="\`${groupName}" type="radio">
-               <label for="\`star${1}-${groupName}"></label>`;
-
-    let doc = new DOMParser().parseFromString(starLayout, "text/html");
-
-    doc.getElementsByTagName("input")[5 - Number(3)].setAttribute("checked", "checked");
 
     const riga = {
       anno: 2025,
-      rating: doc.body.innerHTML,
-      numericalRating: Number(score),
+      rating: Number(score),
       nome: "godzilla",
       immagine: "assets/images/img.png",
       status: "S3, E5"
     }
 
     this.righe.push(riga);
-
-    this.count++;
   }
 
   sortByName(){
@@ -80,17 +58,11 @@ export class TabellaSerieTvComponent {
     this.ratingOrder=true;
 
     if (this.yearOrder){
-      this.righe.sort(function cmp(a,b){
-        if (a.anno<b.anno) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => b.anno - a.anno)
       this.yearOrder=!this.yearOrder;
     }
     else{
-      this.righe.sort(function cmp(a,b){
-        if (a.anno>b.anno) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => a.anno - b.anno)
       this.yearOrder=!this.yearOrder;
     }
   }
@@ -100,17 +72,11 @@ export class TabellaSerieTvComponent {
     this.yearOrder=true;
 
     if (this.ratingOrder){
-      this.righe.sort(function cmp(a,b){
-        if (a.numericalRating<b.numericalRating) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => b.rating - a.rating);
       this.ratingOrder=!this.ratingOrder;
     }
     else{
-      this.righe.sort(function cmp(a,b){
-        if (a.numericalRating>b.numericalRating) return 1;
-        else return -1;
-      })
+      this.righe.sort((a,b) => a.rating - b.rating);
       this.ratingOrder=!this.ratingOrder;
     }
   }
