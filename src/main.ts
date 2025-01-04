88750f00ce2955c9f5ea7f4eb6ade549471f6b36
giuseppefalvo <giuseppefalvo123@gmail.com>
2025-01-04 18:09:27 +0100

    Implementazione schermata login/register
    Problemi con string interpolation ancora da risolvere :(

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { LoginRegisterComponent } from './app/login-register/login-register.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(LoginRegisterComponent)
   .catch((err) => console.error(err));
