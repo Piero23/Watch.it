0303f55382f3f59917b2e8b2acdbad3213a99134
autovetturaincidentata <domenicogaudio34@gmail.com>
2025-01-05 15:23:10 +0100

    Popup delle serie 100% funzionante

--- src/app/profile/tabella-serie-tv/serie-tv-status-selector/serie-tv-status-selector.component.ts
+++ src/app/profile/tabella-serie-tv/serie-tv-status-selector/serie-tv-status-selector.component.ts
@@ -1,37 +1,65 @@
 import {Component, Input, input} from '@angular/core';
 import {ImageCropperComponent} from 'ngx-image-cropper';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 import {TabellaSerieTvComponent} from '../tabella-serie-tv.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-serie-tv-status-selector',
   standalone: true,
   imports: [
     ImageCropperComponent,
-    NgForOf
+    NgForOf,
+    NgIf,
+    FormsModule
   ],
   templateUrl: './serie-tv-status-selector.component.html',
   styleUrl: './serie-tv-status-selector.component.css'
 })
 export class SerieTvStatusSelectorComponent {
-  @Input() image!: string;
   @Input() rowIndex!: number;
+  image!: string;
 
-  currentStatus!: { stagione: number; episodio: number };
+  currentStatus: {stagione: number, episodio: number};
 
-  numeroStagioni: number[]=[];
-  numeroEpisodi: number[]=[];
+  listaStagioni: number[] = [];
+  listaEpisodi: number[][] = [];
 
-  constructor(protected tabella: TabellaSerieTvComponent) {
-    // @ts-ignore
-    this.currentStatus = tabella.righe.at(this.rowIndex).status;
+  selectedEpisodes: number[] = this.listaEpisodi[0]
+
+  constructor(private tabella: TabellaSerieTvComponent) {
     // @ts-ignore
     this.image=tabella.righe.at(this.rowIndex).immagine;
+
     // @ts-ignore
-    this.numeroEpisodi=Array.from({ length: tabella.righe.at(this.rowIndex).total.episodi }, (_, i) => i + 1);
+    const statusOriginale = tabella.righe.at(this.rowIndex).status;
+
+    this.currentStatus = {
+      stagione: statusOriginale.stagione,
+      episodio: statusOriginale.episodio
+    };
+
     // @ts-ignore
-    this.numeroStagioni=Array.from({ length: tabella.righe.at(this.rowIndex).total.stagioni }, (_, i) => i + 1);
+    for (let seasonIndex=1; seasonIndex<=tabella.righe.at(this.rowIndex).stagioni.length; seasonIndex++) {
+      this.listaStagioni.push(seasonIndex);
+      let episodi: number[]=[]
+      // @ts-ignore
+      for (let episodeIndex=1; episodeIndex<=tabella.righe.at(this.rowIndex).stagioni.at(seasonIndex-1).numeroEpisodi; episodeIndex++) {
+        episodi.push(episodeIndex);
+      }
+      this.listaEpisodi.push(episodi);
+    }
+
+    this.selectedEpisodes = this.listaEpisodi[this.currentStatus.stagione - 1];
+    console.log(this.listaStagioni)
+    console.log(this.listaEpisodi)
+  }
+
+  onStagioneChange(stagione: number) {
+    this.currentStatus.stagione = stagione;
+    this.selectedEpisodes = this.listaEpisodi[this.currentStatus.stagione - 1];
+    this.currentStatus.episodio = 1; // Reset dell'episodio
   }
 
 
--- src/app/profile/tabella-serie-tv/tabella-serie-tv.component.ts
+++ src/app/profile/tabella-serie-tv/tabella-serie-tv.component.ts
@@ -25,23 +25,54 @@ export class TabellaSerieTvComponent {
   yearOrder=true;
   ratingOrder=true;
 
-  righe :{anno: number, rating: number, nome: string, immagine: string, status: {stagione: number, episodio: number}, total: {stagioni: number, episodi: number}}[] = [];
   seriesEditor: boolean=false;
   selectedRow: number=0;
 
   constructor(private tmdb: TMDBDataService) {
   }
 
+
+  righe: {
+    anno: number,
+    rating: number,
+    nome: string,
+    immagine: string,
+    status: {
+      stagione: number,
+      episodio: number
+    }
+    stagioni: {
+      numeroStagione: number,
+      numeroEpisodi: number,
+    }[]
+  }[] = []
+
   addrow() {
 
-    const riga: {anno: number, rating: number, nome: string, immagine: string, status: {stagione: number, episodio: number}, total: {stagioni: number, episodi: number}}= {
+    let riga: {
+      anno: number ,
+        rating: number,
+        nome: string,
+        immagine: string,
+        status: {
+          stagione: number,
+          episodio: number
+      }
+      stagioni: {
+        numeroStagione: number,
+        numeroEpisodi: number
+      }[]
+    } = {
       anno: 0,
       rating: 0,
-      nome: '',
-      immagine: '',
-      status: {stagione: 0, episodio: 0},
-      total: {stagioni: 0, episodi: 0}
-    };
+      nome: "",
+      immagine: "",
+      status: {
+        stagione: 1,
+        episodio: 1
+      },
+      stagioni: []
+    }
 
     this.tmdb.getSeriesByID(93405).subscribe({
       next: (data: any)=> {
@@ -51,8 +82,18 @@ export class TabellaSerieTvComponent {
         riga.immagine=data.poster_path
           ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
           : 'URL immagine non disponibile';
-        riga.total.stagioni=data.number_of_seasons;
-        riga.total.episodi=data.number_of_episodes;
+        for (let season=0; season<data.number_of_seasons; season++) {
+          let stagione: {
+            numeroStagione: number,
+            numeroEpisodi: number
+          } = {
+            numeroStagione: 0,
+            numeroEpisodi: 0
+          }
+          stagione.numeroStagione=season+1;
+          stagione.numeroEpisodi=data.seasons.at(season).episode_count;
+          riga.stagioni.push(stagione);
+        }
       }
     });
 
@@ -104,7 +145,7 @@ export class TabellaSerieTvComponent {
     }
   }
 
-  openPopup(image:string, index: number): void {
+  openPopup(index: number): void {
     this.seriesEditor = true;
     this.selectedRow=index;
   }
