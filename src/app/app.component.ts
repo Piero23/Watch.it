5347f017591de9b53330c44ef039eeada0697ce1
giuseppefalvo <giuseppefalvo123@gmail.com>
2025-01-08 16:43:28 +0100

    Creazione schermata commenti (da finire)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {CommentItemComponent} from './comment-item/comment-item.component';
+import {CommentSectionComponent} from './comment-section/comment-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommentItemComponent, CommentSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'WatchedIt';
+  comments: { text: string; rating: number; username: string; profilePic: string }[] = [
+    {
+      text: 'questo film fa cagare',
+      rating: 1,
+      username: 'utente1',
+      profilePic: '',
+    },
+    {
+      text: 'bel film.',
+      rating: 4,
+      username: 'utente2',
+      profilePic: '',
+    },
+    {
+      text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      rating: 3,
+      username: 'utente',
+      profilePic: '',
+    },
+  ];
+
+  isModerator = true;
+
+  addComment(newComment: { text: string; rating: number }) {
+    this.comments.unshift({
+      ...newComment,
+      username: 'Nome Utente',
+      profilePic: '',
+    });
+  }
+
+  deleteComment(index: number) {
+    this.comments.splice(index, 1);
+  }
 }
