1664593af621cdddfa8b4b33dfddce25a8890eed
giuseppefalvo <giuseppefalvo123@gmail.com>
2025-01-14 19:33:11 +0100

    Creazione popup di risposta ai commenti

--- src/app/commenti-contenuto/schermata-commenti/schermata-commenti.component.ts
+++ src/app/commenti-contenuto/schermata-commenti/schermata-commenti.component.ts
@@ -14,34 +14,50 @@ import {CommentSectionComponent} from '../comment-section/comment-section.compon
 })
 export class SchermataCommentiComponent {
   title = 'WatchedIt';
-  comments: { text: string; rating: number; username: string; profilePic: string }[] = [
+  comments: {
+    text: string;
+    rating: number;
+    username: string;
+    profilePic: string;
+    replies: { text: string; rating: number; username: string; profilePic: string }[];
+  }[] = [
     {
       text: 'questo film fa cagare',
       rating: 1,
       username: 'utente1',
-      profilePic: "assets/images/img.png",
+      profilePic: 'assets/images/img.png',
+      replies: [{text: 'ciao', rating: 2, username: 'giuseppe', profilePic: 'assets/images/img.png'}],
     },
     {
       text: 'bel film.',
       rating: 4,
       username: 'utente2',
-      profilePic: "assets/images/utente2.jpg",
-    },
-    {
-      text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-      rating: 3,
-      username: 'utente',
-      profilePic: "assets/images/Avatar.png",
+      profilePic: 'assets/images/utente2.jpg',
+      replies: [],
     },
   ];
 
   isModerator = true;
+  replyingTo: { index: number; username: string; text: string } | null = null;
 
-  addComment(newComment: {text: string; rating: number, username: string, profilePic : string}) {
-    this.comments.unshift(newComment);
+  addComment(newComment: { text: string; rating: number; username: string; profilePic: string }) {
+    if (this.replyingTo) {
+      this.comments[this.replyingTo.index].replies.push({ ...newComment });
+      this.replyingTo = null;
+    } else {
+      this.comments.unshift({ ...newComment, replies: [] });
+    }
   }
 
   deleteComment(index: number) {
     this.comments.splice(index, 1);
+  }
+
+  onReplyToComment(event: { index: number; username: string; text: string }) {
+    this.replyingTo = event;
+  }
+
+  cancelReply() {
+    this.replyingTo = null;
   }
 }
