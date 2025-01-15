14d52e4ebf88c03e2c548648d58bc0b4f815a7e5
autovetturaincidentata <domenicogaudio34@gmail.com>
2025-01-15 19:22:51 +0100

    Fix Service Database

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -31,27 +31,27 @@ export class DatabaseService {
 
   async getCommentiFromContenuto(tipo: string, id: number){
     //tipo: movie/tv
-    return await this.http.get('http://localhost:8080/contenuto/getCommenti/'+tipo+':'+id).toPromise();
+    return await this.http.get('http://localhost:8080/contenuto/getCommenti/'+tipo+'/'+id).toPromise();
   }
 
-  async aggiornaStatus(tipo: string, id: number, status: number){
+  async aggiornaStatus(utente: string, tipo: string, id: number, status: number){
     //tipo: movie/tv
-    await this.http.get('http://localhost:8080/contenuto/updateStatus/'+tipo+':'+id+'-'+status).toPromise();
+    await this.http.get('http://localhost:8080/contenuto/updateStatus/'+utente+'/'+tipo+'/'+id+'/'+status).toPromise();
   }
 
   async aggiornaStagione(id: number, stagione: number, episodio: number){
     //tipo: movie/tv
-    await this.http.get('http://localhost:8080/contenuto/updateSeries/'+id+'-s:'+stagione+'-e:'+episodio).toPromise();
+    await this.http.get('http://localhost:8080/contenuto/updateSeries/'+id+'/'+stagione+'/'+episodio).toPromise();
   }
 
   //Commento
 
   async setAnswer(questionId: number, answerId: number){
-    await this.http.get('http://localhost:8080/commento/setAnswer/q:'+questionId+'-a:'+answerId).toPromise();
+    await this.http.get('http://localhost:8080/commento/setAnswer/'+questionId+'/'+answerId).toPromise();
   }
 
   async saveNewComment(comment_id: number, content_id: number, content_type: boolean, body: string, rating: number, user: string, answers_to: number){
-    await this.http.get('http://localhost:8080/commento//newComment/'+comment_id+'-'+content_id+'-'+content_type+'-'+body+'-'+rating+'-'+user+'-'+answers_to).toPromise();
+    await this.http.get('http://localhost:8080/commento//newComment/'+comment_id+'/'+content_id+'/'+content_type+'/'+body+'/'+rating+'/'+user+'/'+answers_to).toPromise();
   }
 
 }
